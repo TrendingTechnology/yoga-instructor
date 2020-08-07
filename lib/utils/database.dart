64c5988b2ca130696d2ee6cc07ab1a52d26b0907
Souvik Biswas <sbis1999@gmail.com>
2020-08-07 23:03:36 +0530

    Add method for storing user info

--- lib/utils/database.dart
+++ lib/utils/database.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'sign_in.dart';
 
@@ -91,6 +93,31 @@ class Database {
       {'butterfly', 'baddha konasana'},
     ]
   };
+
+  /// For storing user data on the database
+  Future<void> storeUserData({
+    @required String userName,
+    @required String gender,
+    @required int age,
+  }) async {
+    DocumentReference documentReferencer =
+        documentReference.collection('user_info').document(uid);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "image_url": imageUrl,
+      "name": userName,
+      "gender": gender,
+      "age": age,
+    };
+    print('DATA:\n$data');
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await documentReferencer.setData(data).whenComplete(() {
+      print("User Info added to the database");
+      prefs.setBool('details_uploaded', true);
+    }).catchError((e) => print(e));
+  }
 
   /// For easy uploading of tracks to the database
   uploadTracks() async {
