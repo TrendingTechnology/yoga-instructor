2a0e7e8135b5cde12d426a1622fabdce001f2b98
Souvik Biswas <sbis1999@gmail.com>
2020-08-07 22:59:48 +0530

    Add the track names

---
+++ lib/utils/database.dart
@@ -0,0 +1,13 @@
+class Database {
+  List<String> tracks = [
+    'beginners',
+    'power yoga',
+    'morning workout',
+    'immunity booster',
+    'yoga in pregnancy',
+    'insomnia',
+    'cardiovascular yoga',
+    'yoga for migraine',
+    'yoga for asthma',
+  ];
+}
