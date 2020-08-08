3e9a579e12e8678a942fae9e6411b5ffa656545a
Souvik Biswas <sbis1999@gmail.com>
2020-08-08 15:33:31 +0530

    Some speech for the voice assistant

---
+++ lib/speech/output_speech.dart
@@ -0,0 +1,29 @@
+// The output speeches of the voice assistant
+
+final String greetings1 = 'Hi there! I am Sofia';
+final String greetings2 =
+    'As it is your first visit, let\'s get familiarized with the app';
+final String askToStartWithBeginners =
+    'Do you want to start with the beginners track?';
+
+// For starting with the track
+final String startWithTrack = 'Starting with the Triangle pose...';
+
+// Asking to explore
+final String exploreTracks = 'You can explore more tracks on this page';
+
+final String exploreTracksCompletion =
+    'You can explore more tracks on the next page';
+
+// Basic speech input recognition
+final String notRecognized = 'I didn\'t quite get that. Can you please repeat?';
+
+// After completion of a pose
+final String poseCompletion = 'Do you want to continue to the next track?';
+
+// Returning back to the home screen
+final String oneCompletion =
+    'You have received 120 stars, for completing the morning workout track.';
+
+final String oneCompletionString =
+    'You have received ⭐120, for completing the Morning Workout track.';
