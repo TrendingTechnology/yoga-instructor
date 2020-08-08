4e5f3c050db54b955038528a49f1eed6f94bfd2c
Souvik Biswas <sbis1999@gmail.com>
2020-08-08 12:10:27 +0530

    Auth0 sign in button implementation

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -54,6 +54,7 @@ class _LoginPageState extends State<LoginPage> {
               semanticsLabel: 'Cover Image',
             ),
             _signInButton(),
+            _auth0SignIn(),
           ],
         ),
       ),
@@ -107,6 +108,48 @@ class _LoginPageState extends State<LoginPage> {
     } catch (e) {
       print('Error: $e');
     }
+  }
+
+  /// Creating the Auth0 Authentication button
+  Widget _auth0SignIn() {
+    return DecoratedBox(
+      decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          color: Colors.white),
+      child: OutlineButton(
+        highlightColor: Color(0xFFffdbb7),
+        splashColor: Color(0xFFffdbb7),
+        onPressed: !webLogged ? webLogin : null,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        highlightElevation: 0,
+        borderSide: BorderSide(color: Colors.black),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("assets/images/auth0_logo.png"),
+                height: 35.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  'Auth0',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   /// Creating the Google Sign In button
