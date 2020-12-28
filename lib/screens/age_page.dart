b9f976ffdba7defb0e15b38260ce73b35294ea50
Souvik Biswas <sbis1999@gmail.com>
2020-12-28 10:47:22 +0530

    Update age page

--- lib/screens/age_page.dart
+++ lib/screens/age_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sofia/screens/gender_page.dart';
 import 'package:sofia/screens/home_page.dart';
 import 'package:sofia/utils/database.dart';
-import 'package:sofia/utils/sign_in.dart';
 
 /// Widget for generating the Age Screen,
 /// and storing it in the database
@@ -19,15 +17,14 @@ class AgePage extends StatefulWidget {
 }
 
 class _AgePageState extends State<AgePage> {
-  TextEditingController textController;
-  FocusNode textFocusNode;
   Database _database = Database();
 
-  int _age;
-
   String errorString;
-  bool _isEditing = false;
   bool _isStoring = false;
+  int _selectedAgeGroup;
+
+  List<String> _ageGroupList = ['< 20', '20 - 34', '35+'];
+  List<bool> _selectedList = [false, false, false];
 
   AppBar appBar = AppBar(
     centerTitle: true,
@@ -39,28 +36,6 @@ class _AgePageState extends State<AgePage> {
     elevation: 0,
   );
 
-  @override
-  void initState() {
-    super.initState();
-    textController = TextEditingController();
-    textController.text = null;
-    textFocusNode = FocusNode();
-  }
-
-  String _validateString(String value) {
-    value = value.trim();
-
-    if (textController.text != null) {
-      if (value.isEmpty) {
-        return 'Age Can\'t Be Empty';
-      } else if (!isNumeric(value)) {
-        return 'Age should be numeric';
-      }
-    }
-
-    return null;
-  }
-
   bool isNumeric(String s) {
     if (s == null) {
       return false;
@@ -69,26 +44,17 @@ class _AgePageState extends State<AgePage> {
   }
 
   Future<void> _uploadData() async {
-    textFocusNode.unfocus();
     setState(() {
       _isStoring = true;
     });
 
-    var ageString = textController.text.trim();
-    ageString.contains('.')
-        ? ageString = ageString.split('.')[0]
-        : ageString = ageString;
-
-    _age = int.parse(ageString);
-    print('DONE EDITING');
-    print('AGE: $_age');
     await _database.storeUserData(
       userName: widget.userName,
       gender: widget.gender,
-      age: _age,
+      age: _ageGroupList[_selectedAgeGroup],
     );
     _isStoring = false;
-    await Navigator.of(context).push(
+    Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) {
           return HomePage();
@@ -105,124 +71,176 @@ class _AgePageState extends State<AgePage> {
       body: Container(
         color: Color(0xFFfeafb6),
         // Color(0xFFffe6e1), --> color for the other cover
-        child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Container(
-            height: screenSize.height - appBar.preferredSize.height,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    bottom: screenSize.height / 80,
-                  ),
-                  child: Text(
-                    'QUOTE',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.lexendTera(
-                      fontSize: screenSize.width / 30,
-                      color: Colors.black26,
-                    ),
-                  ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: screenSize.height / 80,
+              ),
+              child: Text(
+                'QUOTE',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.lexendTera(
+                  fontSize: screenSize.width / 30,
+                  color: Colors.black26,
                 ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: screenSize.width / 15,
-                      right: screenSize.width / 15,
-                      bottom: screenSize.height / 50),
-                  child: Text(
-                    'The yoga pose you avoid the most you need the most.',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.openSans(
-                      fontSize: screenSize.width / 25,
-                      color: Color(0xFF734435),
-                    ),
-                  ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: screenSize.width / 15,
+                right: screenSize.width / 15,
+                bottom: screenSize.height / 50,
+              ),
+              child: Text(
+                'The yoga pose you avoid the most you need the most.',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.openSans(
+                  fontSize: screenSize.width / 25,
+                  color: Color(0xFF734435),
                 ),
-                Flexible(
-                  child: SvgPicture.asset(
-                    'assets/images/intro_3.svg',
-                    width: screenSize.width,
-                    semanticsLabel: 'Cover Image',
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: screenSize.width / 10,
-                    left: screenSize.width / 10,
-                    bottom: screenSize.height / 10,
-                  ),
-                  child: TextField(
-                    enabled: !_isStoring,
-                    focusNode: textFocusNode,
-                    keyboardType: TextInputType.number,
-                    textInputAction: TextInputAction.done,
-                    style: TextStyle(
-                      color: Color(0xFFc31304),
-                      fontSize: 25,
-                    ),
-                    controller: textController,
-                    cursorColor: Colors.deepOrange,
-                    autofocus: false,
-                    onChanged: (value) {
-                      // setState(() {
-                      //   textController.text = value;
-                      // });
-                      setState(() {
-                        _isEditing = true;
-                      });
-
-                      // _validateString(value);
-                    },
-                    onSubmitted: (value) async {
-                      await _uploadData().catchError(
-                        (e) => print('UPLOAD ERROR: $e'),
-                      );
-                    },
-                    decoration: InputDecoration(
-                      suffix: _isStoring
-                          ? CircularProgressIndicator(
-                              valueColor:
-                                  new AlwaysStoppedAnimation<Color>(Colors.red),
-                            )
-                          : IconButton(
-                              icon: Icon(
-                                Icons.check_circle,
-                                size: 30,
-                                color: Color(0xFFc31304),
-                              ),
-                              onPressed: _isStoring
-                                  ? null
-                                  : () async {
-                                      await _uploadData().catchError(
-                                        (e) => print('UPLOAD ERROR: $e'),
-                                      );
-                                    },
-                            ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Colors.deepOrangeAccent[700]),
+              ),
+            ),
+            Flexible(
+              child: SvgPicture.asset(
+                'assets/images/intro_3.svg',
+                width: screenSize.width,
+                semanticsLabel: 'Cover Image',
+              ),
+            ),
+            SizedBox(height: screenSize.height / 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _selectedList = [true, false, false];
+                      _selectedAgeGroup = 0;
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFf1919c),
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        width: 3,
+                        color: _selectedList[0] ? Color(0xFFed576a) : Color(0xFFf1919c),
                       ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF734435)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        bottom: 16.0,
+                        left: 8.0,
+                        right: 8.0,
                       ),
-                      labelText: 'Enter your age',
-                      labelStyle:
-                          TextStyle(color: Color(0xFF734435), fontSize: 18),
-                      hintText: 'Used for tracking your fitness',
-                      hintStyle: TextStyle(color: Colors.black26, fontSize: 14),
-                      errorText: _isEditing
-                          ? _validateString(textController.text)
-                          : null,
-                      errorStyle:
-                          TextStyle(fontSize: 15, color: Colors.redAccent[800]),
+                      child: Text(
+                        _ageGroupList[0],
+                        style: TextStyle(
+                          fontFamily: 'GoogleSans',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _selectedList = [false, true, false];
+                      _selectedAgeGroup = 1;
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFf1919c),
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        width: 3,
+                        color: _selectedList[1] ? Color(0xFFed576a) : Color(0xFFf1919c),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        bottom: 16.0,
+                        left: 8.0,
+                        right: 8.0,
+                      ),
+                      child: Text(
+                        _ageGroupList[1],
+                        style: TextStyle(
+                          fontFamily: 'GoogleSans',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _selectedList = [false, false, true];
+                      _selectedAgeGroup = 2;
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFf1919c),
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        width: 3,
+                        color: _selectedList[2] ? Color(0xFFed576a) : Color(0xFFf1919c),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        bottom: 16.0,
+                        left: 8.0,
+                        right: 8.0,
+                      ),
+                      child: Text(
+                        _ageGroupList[2],
+                        style: TextStyle(
+                          fontFamily: 'GoogleSans',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20.0,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
-          ),
+            SizedBox(height: screenSize.height / 20),
+            _isStoring
+                ? CircularProgressIndicator(
+                    valueColor: new AlwaysStoppedAnimation<Color>(Color(0xFFed576a)),
+                  )
+                : IconButton(
+                    icon: Icon(
+                      Icons.check_circle,
+                      size: screenSize.width / 10,
+                      color: _selectedAgeGroup != null ? Color(0xFFed576a) : Colors.black12,
+                    ),
+                    onPressed: _selectedAgeGroup != null
+                        ? () async {
+                            await _uploadData().catchError(
+                              (e) => print('UPLOAD ERROR: $e'),
+                            );
+                          }
+                        : null,
+                  ),
+          ],
         ),
       ),
     );
