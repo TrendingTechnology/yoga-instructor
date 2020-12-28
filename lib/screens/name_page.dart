496db8266664afef9b99b89e3534208f1c7dc107
Souvik Biswas <sbis1999@gmail.com>
2020-12-28 09:30:54 +0530

    Improved name page

--- lib/screens/name_page.dart
+++ lib/screens/name_page.dart
@@ -12,9 +12,8 @@ class NamePage extends StatefulWidget {
 }
 
 class _NamePageState extends State<NamePage> {
-  final textController = name != null
-      ? TextEditingController(text: name.split(' ')[0])
-      : TextEditingController();
+  final textController =
+      name != null ? TextEditingController(text: name.split(' ')[0]) : TextEditingController();
   FocusNode textFocusNode;
 
   String _userName;
@@ -36,160 +35,138 @@ class _NamePageState extends State<NamePage> {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: Text(
-      //     'Sofia',
-      //     style: TextStyle(
-      //       color: Colors.deepOrangeAccent[700],
-      //       fontSize: 30
-      //     ),
-      //   ),
-      //   backgroundColor: Color(0xFFffe6e1),
-      //   elevation: 0,
-      // ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            color: Color(0xFFffe6e1),
-            // Color(0xFFffe6e1), --> color for the other cover
-            child: SingleChildScrollView(
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(
-                        bottom: screenSize.height / 80,
-                      ),
-                      child: Text(
-                        'QUOTE',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.lexendTera(
-                          fontSize: screenSize.width / 30,
-                          color: Colors.black26,
-                        ),
+    return GestureDetector(
+      onTap: () {
+        textFocusNode.unfocus();
+      },
+      child: Scaffold(
+        body: Container(
+          color: Color(0xFFffe6e1),
+          // Color(0xFFffe6e1), --> color for the other cover
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: screenSize.height / 80,
+                    ),
+                    child: Text(
+                      'QUOTE',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lexendTera(
+                        fontSize: screenSize.width / 30,
+                        color: Colors.black26,
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: screenSize.width / 15,
-                          right: screenSize.width / 15,
-                          bottom: screenSize.height / 50),
-                      child: Text(
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: screenSize.width / 15,
+                        right: screenSize.width / 15,
+                        bottom: screenSize.height / 50),
+                    child: Text(
                         'Yoga teaches us to cure what need not be endured and endure what cannot be cured.',
                         textAlign: TextAlign.center,
-                        style: GoogleFonts.openSans(
+                        style: TextStyle(
+                          fontFamily: 'GoogleSans',
                           fontSize: screenSize.width / 25,
                           color: Color(0xFFf87473),
+                          letterSpacing: 0.5,
+                        )
+
+                        // GoogleFonts.openSans(
+                        //   fontSize: screenSize.width / 25,
+                        //   color: Color(0xFFf87473),
+                        // ),
                         ),
-                      ),
+                  ),
+                  Flexible(
+                    child: SvgPicture.asset(
+                      'assets/images/cover.svg',
+                      width: MediaQuery.of(context).size.width,
+                      semanticsLabel: 'Cover Image',
                     ),
-                    Flexible(
-                      child: SvgPicture.asset(
-                        'assets/images/cover.svg',
-                        width: MediaQuery.of(context).size.width,
-                        semanticsLabel: 'Cover Image',
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: screenSize.width / 10,
+                      left: screenSize.width / 10,
+                      bottom: screenSize.height / 20,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        right: screenSize.width / 10,
-                        left: screenSize.width / 10,
-                        bottom: screenSize.height / 20,
-                      ),
-                      child: TextField(
-                        focusNode: textFocusNode,
-                        textInputAction: TextInputAction.next,
-                        style: TextStyle(
-                          color: Colors.deepOrangeAccent[700],
-                          fontSize: 25,
-                        ),
-                        controller: textController,
-                        cursorColor: Colors.deepOrange,
-                        onChanged: (value) {
-                          setState(() {
-                            _isEditing = true;
-                          });
-                        },
-                        onSubmitted: (value) {
-                          textFocusNode.unfocus();
-                          _userName = textController.text;
-                          print('DONE EDITING');
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return GenderPage(
-                                  userName: _userName,
-                                );
-                              },
-                            ),
-                          );
-                        },
-                        decoration: InputDecoration(
-                          suffix: IconButton(
-                            icon: Icon(
-                              Icons.check_circle,
-                              size: 30,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width - (screenSize.width / 3),
+                          child: TextField(
+                            focusNode: textFocusNode,
+                            textInputAction: TextInputAction.done,
+                            style: TextStyle(
                               color: Colors.deepOrangeAccent[700],
+                              fontSize: 25,
                             ),
-                            onPressed: () {
+                            controller: textController,
+                            cursorColor: Colors.deepOrange,
+                            onSubmitted: (value) {
                               textFocusNode.unfocus();
+                              setState(() {
+                                _isEditing = true;
+                              });
                               _userName = textController.text;
                               print('DONE EDITING');
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return GenderPage(
-                                      userName: _userName,
-                                    );
-                                  },
-                                ),
-                              );
                             },
+                            decoration: InputDecoration(
+                              suffix: Container(
+                                width: 1,
+                                height: 32,
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.deepOrangeAccent[700]),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blueGrey),
+                              ),
+                              labelText: 'Enter your name',
+                              labelStyle: TextStyle(color: Colors.blueGrey, fontSize: 18),
+                              hintText: 'AI assistant will calling you by this name',
+                              hintStyle: TextStyle(color: Colors.grey, fontSize: 16),
+                              errorText: _isEditing ? _validateString(textController.text) : null,
+                              errorStyle: TextStyle(fontSize: 15, color: Colors.redAccent),
+                            ),
                           ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.deepOrangeAccent[700]),
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blueGrey),
-                          ),
-                          labelText: 'Enter your name',
-                          labelStyle:
-                              TextStyle(color: Colors.blueGrey, fontSize: 18),
-                          hintText:
-                              'AI assistant will calling you by this name',
-                          hintStyle:
-                              TextStyle(color: Colors.grey, fontSize: 16),
-                          errorText: _isEditing
-                              ? _validateString(textController.text)
-                              : null,
-                          errorStyle:
-                              TextStyle(fontSize: 15, color: Colors.redAccent),
                         ),
-                      ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.check_circle,
+                            size: 40,
+                            color: Colors.deepOrangeAccent[700],
+                          ),
+                          onPressed: () {
+                            textFocusNode.unfocus();
+                            _userName = textController.text;
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return GenderPage(
+                                    userName: _userName,
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
-
-          // For Showing a progress indicator
-          // SafeArea(
-          //   child: Container(
-          //     child: LinearProgressIndicator(
-          //       backgroundColor: Color(0xFFffe6e1),
-          //       valueColor: AlwaysStoppedAnimation<Color>(Colors.deepOrangeAccent),
-          //       value: 1/3,
-          //     ),
-          //   ),
-          // ),
-        ],
+        ),
       ),
     );
   }
