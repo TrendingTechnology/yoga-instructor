8e358ef5aa792363dd1574b879880a6fd7bc5347
Souvik Biswas <sbis1999@gmail.com>
2020-12-29 12:27:10 +0530

    Add some new color to palette

---
+++ lib/res/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static final someColor = Colors.white;
+  static final darkShade = Color(0xFF572734);
+  static final lightDarkShade = Color(0xFFBE4C59);
+  static final mediumShade = Color(0xFFF8A4A3);
+  static final lightShade = Color(0xFFFCE0DF);
+}
