f70a2f0eb9347247a82fa6db6ddd8976b75db042
Souvik Biswas <sbis1999@gmail.com>
2021-01-09 13:51:48 +0530

    Add new colors

--- lib/res/palette.dart
+++ lib/res/palette.dart
@@ -7,4 +7,8 @@ class Palette {
   static final mediumShade = Color(0xFFF8A4A3);
   static final lightShade = Color(0xFFFCE0DF);
   static final black = Color(0xFF230107);
+  static final loginBackground = Color(0xFFffead7);
+  static final nameBackground = Color(0xFFffe6e1);
+  static final genderBackground = Color(0xFF5cb798);
+  static final ageBackground = Color(0xFFfeafb6);
 }
