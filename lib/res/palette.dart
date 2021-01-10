1bb1fa56814a01ea320458719debe873fe8ae3ac
Souvik Biswas <sbis1999@gmail.com>
2021-01-10 13:08:09 +0530

    Update a color

--- lib/res/palette.dart
+++ lib/res/palette.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static final someColor = Colors.white;
-  static final darkShade = Color(0xFF572734);
+  static final darkShade = Color(0xFF640226);
   static final lightDarkShade = Color(0xFFcd3871);
   static final mediumShade = Color(0xFFfbe1e0);
   static final lightShade = Color(0xFFFCE0DF);
