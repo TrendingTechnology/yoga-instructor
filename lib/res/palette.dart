d1ba3d902573d7431613ab88fe2fa1b995241c03
Souvik Biswas <sbis1999@gmail.com>
2021-01-10 12:15:54 +0530

    Update color

--- lib/res/palette.dart
+++ lib/res/palette.dart
@@ -4,7 +4,7 @@ class Palette {
   static final someColor = Colors.white;
   static final darkShade = Color(0xFF572734);
   static final lightDarkShade = Color(0xFFcd3871);
-  static final mediumShade = Color(0xFFF8A4A3);
+  static final mediumShade = Color(0xFFfbe1e0);
   static final lightShade = Color(0xFFFCE0DF);
   static final black = Color(0xFF230107);
   static final loginBackground = Color(0xFFffead7);
