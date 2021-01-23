00d4bfbb0fa3fc0ab2175e4fa22d232d4b63180f
Souvik Biswas <sbis1999@gmail.com>
2021-01-23 19:32:10 +0530

    Update the pose storing method

--- lib/utils/database.dart
+++ lib/utils/database.dart
@@ -8,7 +8,7 @@ class Database {
   List<String> tracks = [
     'beginners',
     'power yoga',
-    'morning workout',
+    // 'morning workout',
     'immunity booster',
     'yoga in pregnancy',
     'insomnia',
@@ -36,16 +36,16 @@ class Database {
       {'downward facing dog', 'adho mukha svanasana'},
       {'chair', 'utkatasana'}
     ],
-    'morning workout': [
-      {'mountain', 'tadasana'},
-      {'upward salute', 'urdhva hastasana'},
-      {'standing forward bend', 'hastapadasana'},
-      {'low lunge', 'anjaneyasana'},
-      {'plank', 'phalakasana'},
-      {'four-limbed staff', 'chaturanga dandasana'},
-      {'upward facing dog', 'urdhva mukha svanasana'},
-      {'downward-facing dog', 'adho mukha svanasana'}
-    ],
+    // 'morning workout': [
+    //   {'mountain', 'tadasana'},
+    //   {'upward salute', 'urdhva hastasana'},
+    //   {'standing forward bend', 'hastapadasana'},
+    //   {'low lunge', 'anjaneyasana'},
+    //   {'plank', 'phalakasana'},
+    //   {'four-limbed staff', 'chaturanga dandasana'},
+    //   {'upward facing dog', 'urdhva mukha svanasana'},
+    //   {'downward-facing dog', 'adho mukha svanasana'}
+    // ],
     'immunity booster': [
       {'triangle', 'trikonasana'},
       {'cobra', 'bhujangasana'},
@@ -94,6 +94,53 @@ class Database {
     ]
   };
 
+  List<String> trackDescription = [
+    'This is perfect to get started with yoga. All asanas are safe and simple. You\'ll learn the basic asanas and get ready for a deep dive.',
+    'This track is built to increase stamina. It increases flexibility and promotes weight loss. It also improves circulation and the immune system. Get ready for a mini workout!',
+    // 'Surya Namaskar is a refreshing way to start a day. It is a track of 12 yoga poses which have immense benefits on mind and body. Do it regularly to regularize hormones as well !',
+    'Boost your immunity with this specifically designed track of 5 poses!',
+    'Prenatal yoga has helped women across the globe to have happier and healthier pregnancies. It improves sleep, decreases stress and back pains.',
+    'Having trouble sleeping? No worries! Our 6 step insomnia yoga track will help you tackle insomnia like a pro, with better health as a bonus!',
+    'This track will relax  your blood vessels and lower blood pressure and heart rate, increasing aerobic exercise\'s benefits!',
+    'Tired of chronic headaches? Try out this track to relax and destress and get rid of your headaches in no time!',
+    'Get tired easily of physical exercise? This is a perfect track for you! It consists of 5 yoga asanas performed slow which will make your lungs healthy and strong.'
+  ];
+
+  /// For easy uploading of tracks to the database
+  uploadTracks() async {
+    // beginners
+    int id = 1;
+    poses.forEach((key, value) async {
+      DocumentReference documentReferencer = documentReference.collection('tracks').document(key);
+
+      Map<String, dynamic> name = <String, dynamic>{
+        "id": id,
+        "name": key,
+        "desc": trackDescription[id],
+        "count": value.length,
+      };
+      id = id + 1;
+
+      await documentReferencer.setData(name).whenComplete(() {
+        print("$key track added to the database");
+      }).catchError((e) => print(e));
+
+      value.forEach((element) async {
+        DocumentReference poseDocs =
+            documentReferencer.collection('poses').document(element.elementAt(0));
+
+        Map<String, String> data = <String, String>{
+          "title": element.elementAt(0),
+          "sub": element.elementAt(1),
+        };
+
+        await poseDocs.setData(data).whenComplete(() {
+          print("${element.elementAt(0)} added to the database");
+        }).catchError((e) => print(e));
+      });
+    });
+  }
+
   /// For storing user data on the database
   Future<void> storeUserData({
     @required String userName,
@@ -116,39 +163,6 @@ class Database {
       print("User Info added to the database");
       prefs.setBool('details_uploaded', true);
     }).catchError((e) => print(e));
-  }
-
-  /// For easy uploading of tracks to the database
-  uploadTracks() async {
-    // beginners
-    int id = 1;
-    poses.forEach((key, value) async {
-      DocumentReference documentReferencer = documentReference.collection('tracks').document(key);
-
-      Map<String, dynamic> name = <String, dynamic>{
-        "id": id,
-        "name": key,
-      };
-      id = id + 1;
-
-      await documentReferencer.setData(name).whenComplete(() {
-        print("$key track added to the database");
-      }).catchError((e) => print(e));
-
-      value.forEach((element) async {
-        DocumentReference poseDocs =
-            documentReferencer.collection('poses').document(element.elementAt(0));
-
-        Map<String, String> data = <String, String>{
-          "title": element.elementAt(0),
-          "sub": element.elementAt(1),
-        };
-
-        await poseDocs.setData(data).whenComplete(() {
-          print("${element.elementAt(0)} added to the database");
-        }).catchError((e) => print(e));
-      });
-    });
   }
 
   // Future getProducts() async {
