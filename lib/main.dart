5039f46b5da39fbca2516e241e0b4d61c246b71e
Souvik Biswas <sbis1999@gmail.com>
2021-03-01 14:41:39 +0530

    :zap: Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:sofia/screens/dashboard_page.dart';
-import 'screens/login_page.dart';
-import 'screens/name_page.dart';
-import 'utils/sign_in.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sofia/screens/onboarding_screen.dart';
 
 /// The list of camera types (mainly including: front and back)
 List<CameraDescription> cameras = [];
@@ -13,67 +11,55 @@ void main() async {
     WidgetsFlutterBinding.ensureInitialized();
     cameras = await availableCameras();
   } on CameraException catch (e) {
-    logError(e.code, e.description);
+    print('Error: ${e.code}\nError Message: ${e.description}');
   }
 
   // Starting point of the app
-  runApp(MyApp());
+  runApp(
+    ProviderScope(
+      child: OnboardingScreen(),
+    ),
+  );
 }
-
-/// Prints the camera error, if any occurs
-///
-/// **Inputs:**
-///
-/// [code] - error code
-///
-/// [message] - error message
-///
-/// **Use like:**
-///
-/// ```dart
-/// logError(e.code, e.description);
-/// ```
-///
-void logError(String code, String message) => print('Error: $code\nError Message: $message');
 
 /// First stateless widget to get loaded as soon as
 /// the Dart engine runs
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void initState() {
+//     super.initState();
 
-    getUserInfo();
-  }
+//     getUserInfo();
+//   }
 
-  /// Checks if the user is already logged in,
-  /// and retrieves user info
-  Future getUserInfo() async {
-    await getUser();
-    // await getUid();
-    setState(() {});
-    print(uid);
-  }
+//   /// Checks if the user is already logged in,
+//   /// and retrieves user info
+//   Future getUserInfo() async {
+//     await getUser();
+//     // await getUid();
+//     setState(() {});
+//     print(uid);
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sofia: yoga trainer',
-      // Redirect to the respective page as per the authentication info
-      // Currently using DashboardPage instead of HomePage, to test
-      // the new UI
-      theme: ThemeData(fontFamily: 'GoogleSans'),
-      home: (uid != null && authSignedIn != false)
-          ? detailsUploaded
-              ? DashboardPage() // new page test
-              : NamePage()
-          : LoginPage(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Sofia: yoga trainer',
+//       // Redirect to the respective page as per the authentication info
+//       // Currently using DashboardPage instead of HomePage, to test
+//       // the new UI
+//       theme: ThemeData(fontFamily: 'GoogleSans'),
+//       home: (uid != null && authSignedIn != false)
+//           ? detailsUploaded
+//               ? DashboardPage() // new page test
+//               : NamePage()
+//           : LoginPage(),
+//     );
+//   }
+// }
