1bb9ab9bde77f8c61b97b74cdc00f9e76cf18e07
Souvik Biswas <sbis1999@gmail.com>
2021-03-02 18:18:22 +0530

    Add user info stored widget :blue_heart:

---
+++ lib/widgets/age_widgets/user_info_stored_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sofia/res/palette.dart';
+
+class LoginSignedInWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.check,
+      size: 42,
+      color: Palette.accentDarkPink,
+    );
+  }
+}
