0d7134c63b62492a357fd2d6e6ea43f2cf96bf9d
Souvik Biswas <sbis1999@gmail.com>
2021-03-02 23:42:41 +0530

    Add retrieve pose notifier provider :blue_heart:

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sofia/application/notifiers/auth_current_user_notifier.dart';
 import 'package:sofia/application/notifiers/auth_sign_in_notifier.dart';
+import 'package:sofia/application/notifiers/retrieve_poses_notifier.dart';
 import 'package:sofia/application/notifiers/store_user_data_notifier.dart';
 import 'package:sofia/utils/authentication_client.dart';
 import 'package:sofia/utils/database.dart';
@@ -26,4 +27,8 @@ final authCurrentUserNotifierProvider = StateNotifierProvider(
 
 final storeUserDataNotifierProvider = StateNotifierProvider(
   (ref) => StoreUserDataNotifier(ref.watch(databaseProvider)),
+);
+
+final retrievePosesNotifierProvider = StateNotifierProvider(
+  (ref) => RetrievePosesNotifier(ref.watch(databaseProvider)),
 );
