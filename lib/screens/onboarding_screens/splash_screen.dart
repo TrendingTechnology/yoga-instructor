e4e0cd9f0cf1c96f6f0c54c4fff68a9f715de987
Souvik Biswas <sbis1999@gmail.com>
2021-03-02 18:17:33 +0530

    Update splash screen :rocket:

--- lib/screens/onboarding_screens/splash_screen.dart
+++ lib/screens/onboarding_screens/splash_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:sofia/res/palette.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Palette.loginBackground);
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
+
     return Scaffold(
       body: Container(
         color: Palette.loginBackground,
@@ -14,7 +18,7 @@ class SplashScreen extends StatelessWidget {
               "Sofia",
               style: TextStyle(
                 fontFamily: 'TitilliumWeb',
-                fontSize: 50,
+                fontSize: 60,
                 color: Colors.black,
               ),
             ),
