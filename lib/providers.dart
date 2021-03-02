78b44bf27c6772d0533b1bc35f8adec3e658fcba
Souvik Biswas <sbis1999@gmail.com>
2021-03-02 18:15:50 +0530

    Add database providers :blue_heart:

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sofia/application/notifiers/auth_current_user_notifier.dart';
 import 'package:sofia/application/notifiers/auth_sign_in_notifier.dart';
+import 'package:sofia/application/notifiers/store_user_data_notifier.dart';
 import 'package:sofia/utils/authentication_client.dart';
+import 'package:sofia/utils/database.dart';
 
+// Authentication Providers: ---------------------------------
 final authenticationClientProvider = Provider<AuthenticationClient>(
   (ref) => AuthenticationClient(),
 );
@@ -13,4 +16,13 @@ final authSignInNotifierProvider = StateNotifierProvider(
 
 final authCurrentUserNotifierProvider = StateNotifierProvider(
   (ref) => AuthCurrentUserNotifier(ref.watch(authenticationClientProvider)),
+);
+
+// Databse Providers: -----------------------------------------
+final databaseProvider = Provider<Database>(
+  (ref) => Database(),
+);
+
+final storeUserDataNotifierProvider = StateNotifierProvider(
+  (ref) => StoreUserDataNotifier(ref.watch(databaseProvider)),
 );
