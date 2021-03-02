48c49db69edde9f6da82eafaf32e5f408cb31271
Souvik Biswas <sbis1999@gmail.com>
2021-03-02 23:43:33 +0530

    Remove unwanted comments :rocket:

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -18,13 +18,7 @@ class User {
   String gender;
   String age;
 
-  /// A necessary factory constructor for creating a new User instance
-  /// from a map. Pass the map to the generated `_$UserFromJson()` constructor.
-  /// The constructor is named after the source class, in this case, User.
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
-  /// `toJson` is the convention for a class to declare support for serialization
-  /// to JSON. The implementation simply calls the private, generated
-  /// helper method `_$UserToJson`.
   Map<String, dynamic> toJson() => _$UserToJson(this);
 }
