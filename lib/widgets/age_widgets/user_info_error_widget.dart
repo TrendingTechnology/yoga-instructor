74fae30d0d66bb960d561802a293a216d854dabf
Souvik Biswas <sbis1999@gmail.com>
2021-03-02 22:33:45 +0530

    Update user info widgets :rocket:

--- lib/widgets/age_widgets/user_info_error_widget.dart
+++ lib/widgets/age_widgets/user_info_error_widget.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sofia/providers.dart';
 import 'package:sofia/res/palette.dart';
+import 'package:sofia/widgets/age_widgets/user_info_initial_widget.dart';
 
-class UserInfoStoringWidget extends StatelessWidget {
+class UserInfoErrorWidget extends StatelessWidget {
+  final String errorMessage;
+  final Size screenSize;
+  final String uid;
+  final String imageUrl;
+  final String userName;
+  final String gender;
+
+  const UserInfoErrorWidget({
+    Key key,
+    @required this.errorMessage,
+    @required this.screenSize,
+    @required this.uid,
+    @required this.imageUrl,
+    @required this.userName,
+    @required this.gender,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator(
-      valueColor: AlwaysStoppedAnimation<Color>(
-        Palette.accentDarkPink,
-      ),
+    return Column(
+      children: [
+        UserInfoInitialWidget(
+          screenSize: screenSize,
+          uid: uid,
+          imageUrl: imageUrl,
+          userName: userName,
+          gender: gender,
+        ),
+        Text(
+          errorMessage,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Palette.accentDarkPink,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/widgets/age_widgets/user_info_stored_widget.dart
+++ lib/widgets/age_widgets/user_info_stored_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sofia/res/palette.dart';
 
-class LoginSignedInWidget extends StatelessWidget {
+class UserInfoStoredWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Icon(
