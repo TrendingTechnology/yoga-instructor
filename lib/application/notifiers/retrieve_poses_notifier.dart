6e6325d06f9c049f4b8b7686a8f27295a3857fbf
Souvik Biswas <sbis1999@gmail.com>
2021-03-08 10:14:00 +0530

    Update retrieve poses notifeir :rocket:

--- lib/application/notifiers/retrieve_poses_notifier.dart
+++ lib/application/notifiers/retrieve_poses_notifier.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sofia/application/states/retrieve_poses_state.dart';
 import 'package:sofia/model/pose.dart';
@@ -6,13 +5,15 @@ import 'package:sofia/utils/database.dart';
 
 class RetrievePosesNotifier extends StateNotifier<RetrievePosesState> {
   final Database _database;
+  final String _trackName;
 
-  RetrievePosesNotifier(this._database) : super(RetrievePosesState());
+  RetrievePosesNotifier(this._database, this._trackName)
+      : super(RetrievePosesState());
 
-  Future<void> retrievePoses({@required String trackName}) async {
+  Future<void> retrievePoses() async {
     try {
       state = RetrievePosesState.retrieving();
-      List<Pose> poses = await _database.retrievePoses(trackName: trackName);
+      List<Pose> poses = await _database.retrievePoses(trackName: _trackName);
       state = RetrievePosesState.retrieved(poses);
     } catch (error) {
       state = RetrievePosesState.error(message: 'Error retrieving poses');
