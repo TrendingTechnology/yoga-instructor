d85c6b1252074e2b86c70c74fb3dc7d29ed75491
Souvik Biswas <sbis1999@gmail.com>
2021-03-14 22:55:58 +0530

    Update timer overlay :rocket:

--- lib/screens/timer_overlay.dart
+++ lib/screens/timer_overlay.dart
@@ -2,10 +2,17 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
-import 'package:sofia/screens/preview_screen.dart';
-import 'package:video_player/video_player.dart';
+import 'package:sofia/model/pose.dart';
+import 'package:sofia/res/palette.dart';
 
 class TimerOverlay extends StatefulWidget {
+  final Pose pose;
+
+  const TimerOverlay({
+    Key key,
+    @required this.pose,
+  }) : super(key: key);
+
   @override
   _TimerOverlayState createState() => _TimerOverlayState();
 }
@@ -13,24 +20,12 @@ class TimerOverlay extends StatefulWidget {
 class _TimerOverlayState extends State<TimerOverlay> {
   Timer _timer;
   int _start = 5;
-  VideoPlayerController _controller;
-
-  bool _isInitialized = false;
-
-  Future<void> initializeVideoController() async {
-    _controller = VideoPlayerController.network(
-        'https://stream.mux.com/kiBM5MAziq4wGLnc2GCVixAL8EXYg7wcUDA00VcSkzNM.m3u8.m3u8');
-    await _controller.initialize();
-
-    _isInitialized = true;
-
-    print(_isInitialized);
-  }
+  Pose _currentPose;
 
   @override
   void initState() {
     super.initState();
-    initializeVideoController();
+    _currentPose = widget.pose;
     startTimer();
   }
 
@@ -60,38 +55,87 @@ class _TimerOverlayState extends State<TimerOverlay> {
 
   @override
   Widget build(BuildContext context) {
-    if (_start == 1 && _isInitialized) {
+    Size screenSize = MediaQuery.of(context).size;
+
+    if (_start == 1) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) =>
-                PreviewScreen(videoPlayerController: _controller),
-          ),
-        );
+        // Navigator.of(context).pushReplacement(
+        //   MaterialPageRoute(
+        //     builder: (context) => PreviewScreen(),
+        //   ),
+        // );
+        // Navigator.of(context).pop();
       });
     }
     return Scaffold(
       backgroundColor: Colors.white.withOpacity(0.8),
       body: SafeArea(
-        child: Stack(
-          children: [
-            FlareActor(
-              "assets/rive/loading_1.flr",
-              // alignment: Alignment.center,
-              // fit: BoxFit.fitWidth,
-              animation: "run",
-            ),
-            Center(
-              child: Text(
-                '$_start',
-                style: TextStyle(
-                  fontSize: 100.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      height: screenSize.height,
+                      width: screenSize.height,
+                      child: FlareActor(
+                        "assets/rive/loading_1.flr",
+                        // alignment: Alignment.center,
+                        fit: BoxFit.fitWidth,
+                        animation: "run",
+                      ),
+                    ),
+                    Positioned(
+                      top: screenSize.height * 0.35,
+                      left: screenSize.height / 2 - 20,
+                      child: Text(
+                        '$_start',
+                        style: TextStyle(
+                          fontSize: 100.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
+              Expanded(
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Starting',
+                        style: TextStyle(
+                          fontSize: 36.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        _currentPose.title[0].toUpperCase() +
+                            _currentPose.title.substring(1) +
+                            ' pose',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          color: Palette.lightDarkShade,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
