00e23a5a0daaf8a515ca98138e8747a4e26ccbad
Souvik Biswas <sbis1999@gmail.com>
2021-03-24 15:22:32 +0530

    Add video manager class

---
+++ lib/utils/video_manager.dart
@@ -0,0 +1,11 @@
+import 'package:video_player/video_player.dart';
+
+class VideoManager {
+  static VideoPlayerController videoController;
+
+  static Future<void> initializeVideoController() async {
+    videoController = VideoPlayerController.network(
+        'https://stream.mux.com/kiBM5MAziq4wGLnc2GCVixAL8EXYg7wcUDA00VcSkzNM.m3u8.m3u8');
+    await videoController.initialize();
+  }
+}
