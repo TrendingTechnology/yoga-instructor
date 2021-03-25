594d81fd023d0e9831b0a21c1b2646ab02902d31
Souvik Biswas <sbis1999@gmail.com>
2021-03-25 09:34:57 +0530

    Add animation to score viewer :wink:

--- lib/widgets/recognizer_screen/scrore_viewer_widget.dart
+++ lib/widgets/recognizer_screen/scrore_viewer_widget.dart
@@ -8,25 +8,79 @@ class ScroreViewerWidget extends StatefulWidget {
   const ScroreViewerWidget({
     Key key,
     @required this.accuracy,
+    @required this.accuracyTween,
   }) : super(key: key);
 
   final double accuracy;
+  final Tween<double> accuracyTween;
 
   @override
   _ScroreViewerWidgetState createState() => _ScroreViewerWidgetState();
 }
 
-class _ScroreViewerWidgetState extends State<ScroreViewerWidget> {
+class _ScroreViewerWidgetState extends State<ScroreViewerWidget>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _animation;
+  AnimationController _animationController;
+
+  void setNewPosition() {
+    widget.accuracyTween.begin = widget.accuracyTween.end;
+    _animationController.reset();
+    widget.accuracyTween.end = widget.accuracy;
+    _animationController.forward();
+  }
+  // ...
+
+  // _accuracyTween = Tween(
+  //     begin: 0.0,
+  //     end: _,
+  //   );
+
+  //   controller = AnimationController(
+  //     vsync: this,
+  //     duration: Duration(milliseconds: 20),
+  //   );
+
+  //   animation = _accuracyTween.animate(controller)
+  //     ..addListener(() {
+  //       setState(() {});
+  //     });
+
+  //   controller.forward();
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 20),
+    );
+
+    _animation = widget.accuracyTween.animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _animationController.forward();
+    if (_animation.value != 0.0) {
+      setNewPosition();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    print('ANIM VALUE: ${_animation.value}');
 
     return Stack(
       children: [
         CustomPaint(
-          painter: ShapePainter(
-              width: width, height: height, accuracy: widget.accuracy),
+          painter: AccuracyPainter(
+            width: width,
+            height: height,
+            accuracy: widget.accuracy,
+          ),
           child: SizedBox(
             height: height / 2,
             width: height / 2,
@@ -48,12 +102,12 @@ class _ScroreViewerWidgetState extends State<ScroreViewerWidget> {
   }
 }
 
-class ShapePainter extends CustomPainter {
+class AccuracyPainter extends CustomPainter {
   final double width;
   final double height;
   final double accuracy;
 
-  ShapePainter({
+  AccuracyPainter({
     @required this.width,
     @required this.height,
     @required this.accuracy,
@@ -99,7 +153,7 @@ class ShapePainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
+  bool shouldRepaint(AccuracyPainter oldDelegate) {
+    return accuracy != oldDelegate.accuracy;
   }
 }
